84add04482cf96fcfa16d71c82907b2f7813114a
rafael.abarbosa1 <rafael.abarbosa1@CAS555508W0711.senacsp.edu.br>
2017-08-18 21:40:25 -0300

    versão inicial

---
+++ mavenproject1/src/main/java/br/senac/tads3/pi3a/mavenproject1/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads3.pi3a.mavenproject1;
+
+/**
+ *
+ * @author rafael.abarbosa1
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
